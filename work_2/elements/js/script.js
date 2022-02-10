bb0ad9783e6cb68f42aa238cf0e86bad1bdba0eb
Max <g614380@yandex.ru>
2022-02-10 21:14:34 +0300

    elements

--- work_2/elements/js/script.js
+++ work_2/elements/js/script.js
@@ -1,3 +1,27 @@
 'use strict';
 
-const box = document.getElementById();
+const box = document.getElementById("box");
+
+console.log(box);
+
+//const btns = document.getElementsByTagName('button')[1];
+
+//console.log(btns);
+
+const btns = document.getElementsByTagName('button');
+
+console.log(btns[1]);
+
+const circles = document.getElementsByClassName('circle');
+
+console.log(circles);
+
+const hearts = document.querySelectorAll('.heart');
+
+hearts.forEach(item => {
+    console.log(item);
+});
+
+const oneHeart = document.querySelector('.heart'); // получение первого тега с этим селектором
+
+console.log(oneHeart);
