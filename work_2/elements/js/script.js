011b85d3be0f3e7d790b808bd0c751f01c1a3812
Max <g614380@yandex.ru>
2022-02-13 19:10:33 +0300

    element1

--- work_2/elements/js/script.js
+++ work_2/elements/js/script.js
@@ -25,3 +25,65 @@ hearts.forEach(item => {
 const oneHeart = document.querySelector('.heart'); // получение первого тега с этим селектором
 
 console.log(oneHeart);
+
+const wrapper = document.querySelector('.wrapper');
+
+//  box.style.backgroundColor = 'blue';
+
+//  box.style.width = '500px';
+
+const i = 700;
+
+box.style.cssText = `background-color: blue; width: ${i}px`;
+
+btns[1].style.borderRadius = '100%';
+
+circles[0].style.backgroundColor = 'red';
+
+// for (let i = 0; i < hearts.length; i++){
+//     hearts[i].style.backgroundColor = 'yellow';
+// }
+
+hearts.forEach(item => {
+    item.style.backgroundColor = 'yellow';
+});
+
+const div = document.createElement('div');
+
+//const text = document.createTextNode("Тут был я");
+
+div.classList.add('black');
+
+//document.body.append(div);
+
+wrapper.append(div); // метод вставляет в конец элемента родителя какой то элемент
+
+//wrapper.prepend(div); // метод вставляет в начало родителя какой то элемент
+
+// wrapper.appendChild(div);
+
+// hearts[1].before(div); // вставить до элемента
+
+//hearts[1].after(div); // втавить после элемента
+
+// wrapper.insertBefore(div, hearts[0]);
+
+// circles[0].remove(); // метод удаления элемента со страницы
+
+// wrapper.removeChild(hearts[1]);
+
+// hearts[0].replaceWith(circles[0]); // метод замененяет один элемент на друой
+
+//wrapper.replaceChild(circles[0], hearts[0]);
+
+div.innerHTML = '<h1>Hello world</h1>';
+
+// div.textContent = "Hello";
+
+div.insertAdjacentHTML('afterend', '<h2>Hello</h2>');
+
+
+
+
+
+
