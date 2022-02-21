a4c48c0b07c1b406f5606ae26ec93fdbcfe807f0
Max <g614380@yandex.ru>
2022-02-21 22:47:03 +0300

    tabs

---
+++ Food_dist/js/script.js
@@ -0,0 +1,84 @@
+'use strict';
+// Не мое решение
+// window.addEventListener('DOMContentLoaded', () => {
+
+//     const tabs = document.querySelectorAll('.tabheader__item'),
+//           tabsContent = document.querySelectorAll('.tabcontent'),
+//           tabsParent = document.querySelector('.tabheader__items');
+
+//           function hideTabContent() {
+//               tabsContent.forEach(item => {
+//                    // item.style.display = 'none';
+//                    item.classList.add('hide');
+//                    item.classList.remove('show', 'fade');
+//               });
+//               tabs.forEach(item => {
+//                     item.classList.remove('tabheader__item_active');
+//               });
+//           }
+          
+//           function showTabContent(i = 0) {
+//               //tabsContent[i].style.display = 'block';
+//               tabsContent[i].classList.add('show', 'fade');
+//               tabsContent[i].classList.remove('hide');
+//               tabs[i].classList.add('tabheader__item_active');
+//           }
+
+//           hideTabContent();
+//           showTabContent();
+
+//           tabsParent.addEventListener('click', (event) => {
+
+//             const target = event.target;
+
+//             if(target && target.classList.contains('tabheader__item')){
+//                 tabs.forEach((item, i) => {
+//                     if(target == item){
+//                         hideTabContent();
+//                         showTabContent(i);  
+//                     }
+//                 });
+//             }
+
+//           });
+
+// });
+
+//мое решение
+
+window.addEventListener("DOMContentLoaded", () => {
+
+const tabs = document.querySelectorAll('.tabheader__item'), // получаем псевдомассив divов с табами
+      tabsContent = document.querySelectorAll('.tabcontent'), // получаем псемвдомассив divов с контентом и фотографиями
+      tabsParent = document.querySelector('.tabheader__items'); //получаем родительский div с табами
+
+function hideTabContent () { //функция скрывает контент
+    tabsContent.forEach(item => {
+            item.style.display = 'none';
+    });
+    tabs.forEach(item => {
+            item.classList.remove('tabheader__item_active');
+    });
+}
+function showTabContent(i = 0) { // функция показывает контент
+    tabsContent[i].style.display = 'block';  
+    tabs[i].classList.add('tabheader__item_active');  
+    }
+    
+hideTabContent();
+showTabContent();
+
+tabsParent.addEventListener('click', (event) => {
+    const target = event.target;
+    if (target && target.classList.contains('tabheader__item')){
+        tabs.forEach((item, i) => {
+            if (target == item){
+                hideTabContent();
+                showTabContent(i);
+            }
+        });
+    } 
+});
+
+
+});
