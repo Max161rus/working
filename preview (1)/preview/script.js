ff744f89d9124d3a59e3cdb87dcc701e47fd58a5
Max <g614380@yandex.ru>
2022-02-26 15:26:46 +0300

    scroll

--- preview (1)/preview/script.js
+++ preview (1)/preview/script.js
@@ -1,5 +1,9 @@
 'use strict';
 
+//document это объект в котором содержится вся html структура
+//window это окно в котором показывается документ
+//screen весь видимый монитор (системные панели в него тоже входят)
+
 const box = document.querySelector('.box'),
       btn = document.querySelector('button');
 let i = 0;
@@ -7,11 +11,11 @@ let i = 0;
 // const width = box.clientWidth;
 // const height = box.clientHeight;
 
-// const width = box.offsetWidth;
-// const height = box.offsetHeight;
+const width = box.offsetWidth;
+const height = box.offsetHeight;
 
-const width = box.scrollWidth;
-const height = box.scrollHeight;
+// const width = box.scrollWidth;
+// const height = box.scrollHeight;
 
 
 console.log(width, height);
@@ -23,8 +27,10 @@ btn.addEventListener('click', () => {
     
 });
 
-console.log(box.getBoundingClientRect().top); // метод получающий координаты элемента
-
+console.log(box.getBoundingClientRect().top); // метод получающий координаты элемента, 
+//левая правая верхняя и нижняя границы
+//координаты рассчитываются от левого верхнего угла и левой границы окна
+//и от верхней границы
 const style = window.getComputedStyle(box); // получение компьютед стайлов (стили из CSS) которые применились к элементу
 //стили которые идут со скрипта - инлайн стили
 //инлайн стили прописываются прямо в тег
@@ -37,6 +43,7 @@ console.log(style.display);
 console.log(document.documentElement.clientWidth);
 console.log(document.documentElement.scrollTop);
 
-console.log(window.scrollBy(0, 400));
+console.log(window.scrollBy(0, 400));//аргументы x и  y относительно от текущего положения
 
-console.log(window.scrollTo(0, 400));
+console.log(window.scrollTo(0, 400));//аргуметны х и у относительно начала старницы
+
