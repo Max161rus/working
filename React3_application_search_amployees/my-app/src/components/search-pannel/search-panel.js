6f926fb77c40ec4d7023640c5c372c5cba246e70
Max <g614380@yandex.ru>
2022-06-22 21:38:13 +0300

    the_end_applicationReact

--- React3_application_search_amployees/my-app/src/components/search-pannel/search-panel.js
+++ React3_application_search_amployees/my-app/src/components/search-pannel/search-panel.js
@@ -1,11 +1,32 @@
+import { Component } from 'react';
 import './search-panel.css'
 
-const SearchPanel = () => {
+class SearchPanel extends Component{
+    constructor(props){
+        super(props);
+
+        this.state = {
+            term: ''
+        }
+    }
+
+onUpdateSearchRules = (e) => {
+    const term = e.target.value;
+    this.setState({term}); // установка локального состояния
+    this.props.onUpdateSearch(term);
+    
+}
+
+render(){
     return(
         <input type="text"
         className="form-control search-input"
-        placeholder="Найти сотрудника"/>
+        placeholder="Найти сотрудника"
+        value={this.state.term}
+        onChange={this.onUpdateSearchRules}/>
     )
+}
+    
 }
 
 export default SearchPanel;
