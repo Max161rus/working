3f221dc8dfc42334f36947e478d938dd6f853b98
Max <g614380@yandex.ru>
2022-06-29 21:19:21 +0300

    styledComponent

--- React4_props/my-app/src/App.js
+++ React4_props/my-app/src/App.js
@@ -1,7 +1,38 @@
-import {Component} from 'react';
+import {Component} from "react";
+
+import styled from "styled-components"
+
 import './App.css';
 
-class WhoAmi extends Component {
+const EmpItem = styled.div`
+  padding: 20px;
+  margin-bottom: 15px;
+  border-radius: 5px;
+  box-shadow: 5px 5px 10px rgba(0,0,0, .2);
+  a{
+    display: block;
+    margin: 10px 0 10px 0;
+    color: ${props => props.active ? "orange" : "black"};
+  }
+  input {
+    display: block;
+    margin-top: 10px;
+  }
+`;
+
+const Header = styled.h2`
+  font-size: 22px;
+`;
+
+export const Button = styled.button`
+  display: block;
+  padding: 5px 15px;
+  background-color: gold;
+  border: 1px solid rgba(0,0,0, .2);
+  box-shadow: 5px 5px 10px rgba(0,0,0, .2);
+`;
+
+class WhoAmI extends Component{
   constructor(props){
     super(props);
     this.state = {
@@ -9,53 +40,56 @@ class WhoAmi extends Component {
       text: "+++",
       position: ""
     }
-   
+    //this.nextYear = this.nextYear.bind(this);
   }
 
-  nextYear = () =>  {
+  nextYear = () => {
+    console.log("+++");
     this.setState(state => ({
       years: state.years + 1
     }))
   }
 
   commitInputChanges = (e, color) => {
-    console.log(color)
+    console.log(color);
     this.setState({
       position: e.target.value
     })
   }
 
+  render() {
+    const {name, surname, link} = this.props;
+    const {position, years} = this.state;
 
- render() {
-   const {name, surname, link} = this.props;
-   const {position, years} = this.state;
-      return(
-        <div>
-          <button onClick={this.nextYear}>{this.state.text}</button>
-          <h1>My name is {name}, 
-              surname - {surname}, 
-              age - {years}, 
-              position - {position}</h1>
-          <a href={link}>My profile</a>
-          <form>
-              <span>Введите должность</span>
-              <input type="text" onChange={(e) => this.commitInputChanges(e, "some color")}/>
-          </form>
-        </div>
-      )
- }
-  
+    
+    return(
+    <EmpItem active>
+    <Button onClick={this.nextYear}>{this.state.text}</Button>
+      <Header>My name is {name}, surname - {surname},
+          age - {years},
+          position - {position} </Header>
+      <a href={link}>My profile</a>
+      <form>
+        <span>Введите должность</span>
+        <input type="text" onChange={(e) => this.commitInputChanges(e, "some color")}/>
+      </form>
+    </EmpItem>
+    );
+    };
 }
 
-
+const Wrapper = styled.div`
+  width: 600px;
+  margin: 80px auto 0 auto;
+  background-color: white;
+`;
 
 function App() {
   return (
-    <div className="App">
-        <WhoAmi name="John" surname="Smith"  link="facebook.com"/>
-        <WhoAmi name="Alex" surname="Shepard"  link="vk.com"/>
-        
-    </div>
+    <Wrapper>
+      <WhoAmI name='John' surname="Smith" link="facebook.com"/>
+      <WhoAmI name="Alex" surname="Shepard" link="vk.com"/>
+    </Wrapper>
   );
 }
 
